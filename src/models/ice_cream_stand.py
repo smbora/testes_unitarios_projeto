f59e9b84791dc2fc4a25e41b4afeae64045ecf75
Igor Nogueira de Oliveira <igor@acumera.com>
2023-10-12 13:04:55 -0300

    Adding some fixes

--- src/models/ice_cream_stand.py
+++ src/models/ice_cream_stand.py
@@ -4,58 +4,40 @@ from src.models.restaurant import Restaurant
 class IceCreamStand(Restaurant):
     """Um tipo especializado de restaurante."""
 
-    def __init__(self, restaurant_name, cuisine_type, flavors_list):
+    def __init__(self, restaurant_name, cuisine_type, flavors_list = None):
         """
         Inicialize os atributos da classe pai.
         Em seguida, inicialize os atributos específicos de uma sorveteria.
         """
         super().__init__(restaurant_name, cuisine_type)
-        self.flavors = [] #Inicializar self.flavors como uma lista vazia
-        
-    # def flavors_available(self):
-    #     """Percorra a lista de sabores disponíveis e imprima."""
-    #     if self.flavors:
-    #         return (f"\nNo momento temos os seguintes sabores de sorvete disponíveis: {self.flavors}")
-    #         for flavor in self.flavors:
-    #             print(f"\t-{flavor}")
-    #
-    #     else:
-    #         print("Estamos sem estoque atualmente!")
+        self.flavors = flavors_list if flavors_list else []
 
     def flavors_available(self):
         """Percorra a lista de sabores disponíveis e imprima."""
-
-        if len(self.flavors) == 0:
+        if not self.flavors:
             return "Estamos sem estoque atualmente!"
-
-        if self.flavors:
+        else:
             flavors_list = ", ".join(self.flavors)
             return f"No momento temos os seguintes sabores de sorvete disponíveis: {flavors_list}"
 
     def find_flavor(self, flavor):
         """Verifica se o sabor informado está disponível."""
 
-        #if self.flavors:
-        if  len(self.flavors) == 0:
+        if not flavor or not isinstance(flavor, str):
+            return "Sabor informado não é valido!"
+        if not self.flavors:
             return "Estamos sem estoque atualmente!"
-
         if flavor in self.flavors:
-            return (f"Temos {flavor} no momento !")
+            return f"Temos {flavor} no momento !"
         else:
-            return (f"Não temos {flavor} no momento!")
-        #else:
-            #print("Estamos sem estoque atualmente!")
+            return f"Não temos {flavor} no momento!"
 
     def add_flavor(self, flavor):
         """Add o sabor informado ao estoque."""
-        #if self.flavors:
-
+        if not flavor or not isinstance(flavor, str):
+            return "Sabor informado não é valido!"
         if flavor in self.flavors:
-            return ("\nSabor já disponivel!")
-
+            return "Sabor já disponivel!"
         else:
             self.flavors.append(flavor)
-            return (f"{flavor} adicionado ao estoque!")
-
-        #else:
-            #print("Estamos sem estoque atualmente!")
+            return f"{flavor} adicionado ao estoque!"
--- src/models/restaurant.py
+++ src/models/restaurant.py
@@ -16,49 +16,44 @@ class Restaurant:
 
     def open_restaurant(self):
         """Imprima uma mensagem indicando que o restaurante está aberto para negócios."""
-        # if not self.open:
-        #     self.open = False #self.open = True ?
-        #     self.number_served = -2
-        #     print(f"{self.restaurant_name} agora está aberto!")
-        # else:
-        #     print(f"{self.restaurant_name} já está aberto!")
         if self.open:
             return f"{self.restaurant_name} já está aberto!"
-        self.open = True
-        self.number_served = 0
-        return f"{self.restaurant_name} agora está aberto!"
+        else:
+            self.open = True
+            self.number_served = 0
+            return f"{self.restaurant_name} agora está aberto!"
+
     def close_restaurant(self):
         """Imprima uma mensagem indicando que o restaurante está fechado para negócios."""
         if self.open:
             self.open = False
             self.number_served = 0
-            print(f"{self.restaurant_name} agora está fechado!")
+            return f"{self.restaurant_name} agora está fechado!"
         else:
-            print(f"{self.restaurant_name} já está fechado!")
+            return f"{self.restaurant_name} já está fechado!"
 
     def set_number_served(self, total_customers):
         """Defina o número total de pessoas atendidas por este restaurante até o momento."""
+        if not total_customers or not isinstance(total_customers, int):
+            return "Valor informado não é valido!"
         if self.open:
             self.number_served = total_customers
-            #print(f"{self.number_served} foram atendidas por este restaurante até o momento")
         else:
-            print(f"{self.restaurant_name} está fechado!")
+            return f"{self.restaurant_name} está fechado!"
 
     def increment_number_served(self, more_customers):
         """Aumenta número total de clientes atendidos por este restaurante."""
+        if not more_customers or not isinstance(more_customers, int) or more_customers < 0:
+            return "Valor informado não é valido!"
         if self.open:
-            self.number_served = more_customers
-            #print(f"{self.number_served} clientes foram atendidos por este restaurante)
+            self.number_served += more_customers
+            return f"{more_customers} foram adicionados aos {self.number_served - more_customers} clientes já atendidos"
         else:
-            print(f"{self.restaurant_name} está fechado!")
+            return f"{self.restaurant_name} está fechado!"
 
-    # def increment_number_served(self, more_customers):
-    #     """Aumenta número total de clientes atendidos por este restaurante."""
-    #     if self.open:
-    #         if more_customers > 0:
-    #             self.number_served += more_customers
-    #                 print(f"{more_customers} clientes foram atendidos por {self.restaurant_name} até o momento.")
-    #         else:
-    #             print("A quantidade de clientes a ser incrementada deve ser um número positivo.")
-    #      else:
-    #         print(f"{self.restaurant_name} está fechado!") """
+    def get_number_served(self):
+        """Informa o número total de pessoas atendidas por este restaurante até o momento."""
+        if self.open:
+            return f"{self.number_served} clientes foram atendidos ate o momento"
+        else:
+            return f"{self.restaurant_name} está fechado!"
